7b4fff7a7e7cb8d07f627aed18c74bf31a60eac6
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2021-07-07 18:51:47 +0800

    Adding "key" prop to article list.

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -8,7 +8,7 @@ const ArticleList = ({ articles }) => {
       ${stylesUtility["gap_20"]}
     `}>
       {articles.map((article) => (
-        <div className={`
+        <div key={article.id} className={`
           ${stylesUtility["flex_item_12"]}
           ${stylesUtility["flex_item_xs_6"]}
           ${stylesUtility["flex_item_md_4"]}
